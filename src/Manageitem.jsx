4ea20ba20d41004be2338f1991124f1faf9ccb3e
arunkumar0512 <140463247+arunkumar0512@users.noreply.github.com>
2024-01-06 00:47:41 +0530

    Update Manageitem.jsx

--- src/Manageitem.jsx
+++ src/Manageitem.jsx
@@ -1,77 +1,4 @@
-// import React from 'react';
-// import axios from 'axios';
-// import useProducts from './Useproduct.jsx';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faXmark } from '@fortawesome/free-solid-svg-icons';
-// import { toast } from 'react-toastify';
-// import { useNavigate } from 'react-router-dom';
-// import './Manageitem.css'
-
-// const Manageitem = () => {
-//     const [products] = useProducts();
-//     const navigate = useNavigate();
-
-//     const handleRemove = id => {
-//         const proceed = window.confirm('Are You Sure to Delete?');
-//         if (proceed) {
-//             fetch(`http://localhost:4001/api/delete/product/${id}`, {
-//                 method: 'DELETE',
-//             })
-//                 .then(res => {
-                    
-//                     window.location.reload()
-                    
-                    
-                    
-//                     res.json()})
-//                 .then(data => {
-//                     toast('Product Successfully Deleted', data);
-//                 })
-//         }
-//     }
-
-//     const navigateAddItem = () => {
-//         navigate('/Additem');
-//     }
-//     return (
-//         <div>
-//             <h1 style={{marginTop:'200px'}}>Inventory Management Page</h1>
-//             <div className='table-container'>
-//                 <table className='table'>
-//                     <thead>
-//                         <tr style={{fontSize:'50px'}}>
-//                             <th>Name</th>
-//                             <th>size</th>
-//                             <th>size</th>
-//                             <th>quantity</th>
-//                             <th>Delete</th>
-//                         </tr>
-//                     </thead>
-//                     <tbody>
-//                         {
-//                             products.map((product) => {
-//                                 return (
-//                                     <tr key={product._id}>
-//                                         <td>{product.name}</td>
-//                                         <td>{product.size}</td>
-//                                         <td>{product.size}</td>
-//                                         <td>{product.quantity}</td>
-                                        
-//                                         <td><button onClick={() => handleRemove(product._id)}><FontAwesomeIcon icon={faXmark}></FontAwesomeIcon></button></td>
-//                                     </tr>
-//                                 )
-//                             })
-//                         }
-//                     </tbody>
-//                 </table>
-
-//             </div>
-//             <button onClick={navigateAddItem} className='btn btn-success' style={{ backgroundColor:'grey'}}>Add new item</button>
-//         </div>
-//     );
-// };
-
-// export default Manageitem;   
+ 
 
 
 
@@ -82,7 +9,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark, faXmarkSquare } from '@fortawesome/free-solid-svg-icons';
 import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
-import './manageitem.css'
+import './Manageitem.css'
 
 const Manageitem = () => {
     const [products] = useProducts();
